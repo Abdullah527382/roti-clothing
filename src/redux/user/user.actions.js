5177d3c73c8e1f8b03b1d8cbf9b80b586f9d3162
abdullah <abdullah527382@gmail.com>
2021-01-24 17:23:34 +1100

    updating app to redirect to home once signed in and created file for userActionTypes

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,8 @@
+import { UserActionTypes } from "./user.types";
+
 // Function which gets the user but returns an action object
 
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
