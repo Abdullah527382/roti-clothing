1c3701fba4cf7f48e44f6efec7f11c070a6f2978
abdullah <abdullah527382@gmail.com>
2021-02-02 10:57:16 +1100

    updated stripe integration and changed the favicon, tab title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IGBcfI8Pb4YoI1NGcXOiMIEH7B7ksiBurkCLRgSMPuFPDmMMKiSpCzE9PsgUlaQP8cx9ndxgWBHV40KJ8Axr0jy00NP0jOWOM";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="ROTI Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
