c6e2b2074db64665d6d2c535d5adfe294bb4d533
abdullah <abdullah527382@gmail.com>
2021-02-02 19:51:43 +1100

    fixed store.js export errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,5 +14,3 @@ const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
-
-export default { store, persistor };
